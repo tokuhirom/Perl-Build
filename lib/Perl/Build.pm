32200eaa4955f29829515235d5627b3cf512ee50
tokuhirom <tokuhirom@gmail.com>
2013-01-24 11:41:44 +0900

    support relative path to install from tar ball

--- lib/Perl/Build.pm
+++ lib/Perl/Build.pm
@@ -94,10 +94,10 @@ sub extract_tarball {
     my $tarx =
         ($^O eq 'solaris' ? 'gtar ' : 'tar ') .
         ( $dist_tarball =~ m/bz2$/ ? 'xjf' : 'xzf' );
-    my $extract_command = "cd @{[ $destdir ]}; $tarx $dist_tarball";
+    my $extract_command = "cd @{[ $destdir ]}; $tarx @{[ File::Spec->rel2abs($dist_tarball) ]}";
     system($extract_command) == 0
         or die "Failed to extract $dist_tarball";
-    $dist_tarball =~ s{.*/([^/]+)\.tar\.(?:gz|bz2)$}{$1};
+    $dist_tarball =~ s{(?:.*/)?([^/]+)\.tar\.(?:gz|bz2)$}{$1};
     return "$destdir/$dist_tarball"; # Note that this is incorrect for blead
 }
 
