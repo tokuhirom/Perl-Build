afab91f450d5041e6a6dfdd74c6cd641d8fba804
tokuhirom <tokuhirom@gmail.com>
2013-04-01 10:46:12 +0900

    Updated license terms.

--- lib/Perl/Build.pm
+++ lib/Perl/Build.pm
@@ -410,7 +410,33 @@ Tokuhiro Matsuno E<lt>tokuhirom@gmail.comE<gt>
 
 =head1 LICENSE
 
-This software takes most of the code from L<App::perlbrew>.
+Copyright (C) Tokuhiro Matsuno
 
-Perl::Build uses same license with perlbrew.
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+This software takes lot of the code from L<App::perlbrew>. App::perlbrew's license is:
+
+    The MIT License
+
+    Copyright (c) 2010,2011 Kang-min Liu
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+
 
