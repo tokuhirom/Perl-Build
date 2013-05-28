7ee534ba90c3cd136e6a92a9d57c7d2fc7ff79ac
tokuhirom <tokuhirom@gmail.com>
2013-05-28 23:32:55 +0900

    Checking in changes prior to tagging of version 0.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index dde1ff5..c43b686 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension App::perlbuild
    
     {{$NEXT}}
    
    +0 2013-05-28T14:32:39Z
    +
    +    - Added `--definitions` option. This option is same as ruby-build.
    +      (tokuhirom)
    +
     0.20 2013-05-17T00:46:48Z
    
         - use 'test_harness' for parallel testing.

--- lib/Perl/Build.pm
+++ lib/Perl/Build.pm
@@ -4,7 +4,7 @@ use warnings;
 use utf8;
 
 use 5.008002;
-our $VERSION = '0.20';
+our $VERSION = 'y';
 
 use Carp ();
 use File::Basename;
--- lib/Perl/Build/Built.pm
+++ lib/Perl/Build/Built.pm
@@ -5,7 +5,7 @@ use warnings;
 use utf8;
 
 use 5.008002;
-our $VERSION = '0.20';
+our $VERSION = 'y';
 
 use Carp ();
 use File::Spec::Functions qw( catdir catfile );
