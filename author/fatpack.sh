9d3a0b0f0d54a9130222c9596fa0426fd7ca5e7c
Masahiro Nagano <kazeburo@gmail.com>
2014-09-22 22:31:10 +0900

    perlstrip breaks Pod/Simple/BlackBox.pm #33

--- author/fatpack.sh
+++ author/fatpack.sh
@@ -9,7 +9,7 @@ fatpack packlists-for `cat fatpacker.trace` >packlists
 fatpack tree `cat packlists`
 
 if type perlstrip >/dev/null 2>&1; then
-    find fatlib -type f -name '*.pm' | xargs -n1 perlstrip -s
+    find fatlib -type f -name '*.pm' |grep -v 'Pod/Simple/BlackBox.pm'|xargs -n1 perlstrip -s
 fi
 
 (echo "#!/usr/bin/env perl"; fatpack file; cat $SRC) > $DST
