0647429881d9bafc39dc818f37dac812a6b6d097
Tokuhiro Matsuno <tokuhirom@gmail.com>
2018-05-19 08:00:34 +0000

    curl in installation instructions needs -L close #68

--- lib/Perl/Build.pm
+++ lib/Perl/Build.pm
@@ -410,7 +410,7 @@ Perl::Build - perl builder
 =head1 CLI interface without dependencies
 
     # perl-build command is FatPacker ready
-    % curl https://raw.githubusercontent.com/tokuhirom/Perl-Build/master/perl-build | perl - 5.16.2 /opt/perl-5.16/
+    % curl -L https://raw.githubusercontent.com/tokuhirom/Perl-Build/master/perl-build | perl - 5.16.2 /opt/perl-5.16/
 
 =head1 CLI interface
 
